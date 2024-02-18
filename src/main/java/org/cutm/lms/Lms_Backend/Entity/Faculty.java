ca828a9a2b63e33b43f6e414551c579f561270a2
debasisnishank <nishankdebasis4@gmail.com>
2024-02-18 20:43:00 +0530

    Entity Column added

---
+++ src/main/java/org/cutm/lms/Lms_Backend/Entity/Faculty.java
@@ -0,0 +1,18 @@
+package org.cutm.lms.Lms_Backend.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Faculty {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long facultyId;
+	private String facultyEmail;
+	private String facultyPasswd;
+	@OneToOne
+	private Branch branch;
+    @ManyToOne
+    @JoinColumn(name = "courseId")
+	private Course course;
+
+}
